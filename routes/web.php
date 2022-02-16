7bffc26068821a728823f7ca091344371a165982
RafsDuarte <rafinha.lduarte@hotmail.com>
2022-02-16 14:15:36 -0300

    Criacao database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $helloWorld = 'Hello World';
+
+    
+    return view('welcome', compact('helloWorld'));
 });
 
 Auth::routes();
